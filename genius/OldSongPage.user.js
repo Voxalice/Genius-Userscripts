22ba30f7fc17ee2d146faa029f8a09ad0ae2c701
Voxalice <64228713+Voxalice@users.noreply.github.com>
2022-08-05 09:14:53 -0500

    OldSongPage v2 revert
    v3 redirects too much

--- genius/OldSongPage.user.js
+++ genius/OldSongPage.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Genius - Bring Back Old Song Page
 // @namespace    https://github.com/Voxalice/
-// @version      3
-// @description  Disables new song page.
+// @version      2
+// @description  Disables new song page
 // @author       Voxalice
 // @match        https://genius.com/*
 // @icon         https://genius.com/favicon.ico
+// @grant        none
 // ==/UserScript==
-!location.search&&(location.href+="?bagon=1")
+window.location.href.endsWith("lyrics")&&(window.location.href+="?bagon=1");
