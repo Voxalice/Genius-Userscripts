ab43bf5a6314fa3e2a61bf7f8b987977a7c7f0b4
Voxalice <64228713+Voxalice@users.noreply.github.com>
2022-11-27 09:25:49 -0600

    OldSongPage v3
    Smarter new song page detection

--- genius/OldSongPage.user.js
+++ genius/OldSongPage.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Genius - Bring Back Old Song Page
 // @namespace    https://github.com/Voxalice/
-// @version      2
-// @description  Disables new song page
+// @version      3
+// @description  Redirects to old song page
 // @author       Voxalice
 // @match        https://genius.com/*
 // @icon         https://genius.com/favicon.ico
 // @grant        none
 // ==/UserScript==
-window.location.href.endsWith("lyrics")&&(window.location.href+="?bagon=1");
+document.body.innerText.indexOf("We need your help to continue improving contributor features")>9&&(window.location.href+="?bagon=1");
